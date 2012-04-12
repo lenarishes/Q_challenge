1b420744c3716f99486385f3b7f161309606a613
Elena Rishes <lena4848@gmail.com>
2012-04-12 14:38:49 -0700

    Implementation of Dijkstra's algorithm

--- Dijkstra.py
+++ Dijkstra.py
@@ -2,62 +2,105 @@ import collections, heapq
 
 class Digraph(object):
     def __init__(self):
-        self.nodes = collections.defaultdict(list)
+        """
+        Adjacency lists representation of directed graph
+        
+        edge: dict, for each node v contains a list of edge u such that (v,u) is an outgoing edge
+        edges: dict, for each edge (v,u) contains its length         
+        """
+        self.edge = collections.defaultdict(list)
         self.edges = {}
     
     def addEdge(self, tail, head, dist):
-        self.nodes[tail].append(head)
-        if head not in self.nodes.keys():
-            self.nodes[head] = []
+        """
+        Add edge 'tail', 'head' and the edge of length 'dist' connecting them to the graph
+        
+        tail: string 
+        head: string
+        dist: int
+        """
+        self.edge[tail].append(head)
+        if head not in self.edge.keys():
+            self.edge[head] = []
         edge = (tail, head)
         self.edges[edge] = dist
         
     def getNodes(self):
-        return self.nodes.keys()
+        """
+        Get the list of all graph edge
+        """
+        return self.edge.keys()
         
     def countNodes(self):
-        return len(self.nodes.keys())
+        """
+        Get the number of all graph edge
+        """
+        return len(self.edge.keys())
         
     def edgeLength(self, a, b):
+        """
+        Get the length of edge going from node 'a' to node 'b'.
+        Returns 0 if there is no such edge.
+        
+        a: string
+        b: string
+        """
         edge = (a,b)
         if edge in self.edges:
             return self.edges[edge]        
         return 0
     
     def getHeads(self, tail):
-        return self.nodes[tail]
+        """
+        Get the list of edge that can be reached from 'tail'
+        
+        tail: string
+        """
+        return self.edge[tail]
         
     def __str__(self):
         result = ""
-        for t, heads in self.nodes.items():
+        for t, heads in self.edge.items():
             for h in heads:
                 string = "%s %s %d" % (t, h, self.edges[(t,h)]) 
                 result = result + string + "\n"
         return result  
 
 def findPath(G, start, goal):
+    """
+    Find shortest path from start to goal.
+    Where start and goal are edge of a directed weighted graph
+    
+    G: a Digraph object
+    start: string
+    goal: string
+    returns: a list of tuples representing the edges in the path
+    """
+    # contains lengths of shortest paths to the edge found so far
     dist = collections.defaultdict(int)
-    dist[start] = 0    
+    dist[start] = 0
+    # contains the shortes paths to the edge found so far    
     path = collections.defaultdict(list)
-    #processed nodes
+    #processed edge
     seen = [start]
-    #heap to choose minimal from available paths
+    #heap to choose the minimal path from the frontier
     h = []
     if start == goal:
         return []
               
     while len(seen) < G.countNodes():
+        #expand the node which we added last
         tail = seen[-1]    
         for head in G.getHeads(tail):
             if head not in seen:
                 heapq.heappush(h, (dist[tail] + G.edgeLength(tail, head), (tail, head)))
         #until we find a shortest path to a node not reached yet
         while True:
-            distance, nodes = heapq.heappop(h)
-            b,e = nodes[0], nodes[1]
+            distance, edge = heapq.heappop(h)
+            b,e = edge[0], edge[1]
             if e not in seen:
                 dist[e] = distance
-                path[e] = path[b] + [nodes]
+                path[e] = path[b] + [edge]
                 seen.append(e)
                 if e == goal:
                     return path[e]
@@ -65,13 +108,3 @@ def findPath(G, start, goal):
                     break                
     #goal is unreachable
     return None
-
-def getGraphFromFile(path):
-    graph = Digraph()
-    for line in open(path, 'r'):
-        line = line.split()
-        graph.addEdge(line[0], line[1], int(line[2]))
-        graph.addEdge(line[1], line[0], int(line[2]))
-    return graph
-    
-print findPath(getGraphFromFile('map.txt'), 'Paris', 'Barcelona')
